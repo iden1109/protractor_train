6bcc50e7f19663bb00c5c94d166f77492fd7eb3a
iden <iden1109@gmail.com>
2019-11-16 16:07:10 +0800

    實戰演練:練習複雜 DOM 定位運用

--- src/events/search-event.e2e-spec.ts
+++ src/events/search-event.e2e-spec.ts
@@ -1,5 +1,29 @@
+import { browser, by, element, $, $$, ExpectedConditions as EC } from 'protractor';
 
 describe('search a event', () => {
 
+    beforeEach(async () => {
+        await browser.waitForAngularEnabled(true);
+    });
+
+    it('should open activity page', async () => {
+        await browser.get('/events');
+        const url = await browser.getCurrentUrl();
+        expect(url).toContain('/events');
+    });
+
+    it('should return 3 records when search "Angular"', async () => {
+        await element(by.name('searchTerm')).sendKeys('Angular');
+        await element(by.buttonText('搜尋')).click();
+        const groupItems = element.all(by.className('list-group-item'));
+        const resultCount = await groupItems.count();
+        expect(resultCount).toEqual(3);
+    });
+
+    it('should show "ANGULAR 7 開發實戰" when click item ANGULAR 開發實戰', async () => {
+        await element(by.linkText('Angular 實戰開發')).click();
+        const title = await element(by.tagName('h2')).getText();
+        expect(title).toContain('ANGULAR 7 開發實戰：新手入門篇');
+    });
 });
 
