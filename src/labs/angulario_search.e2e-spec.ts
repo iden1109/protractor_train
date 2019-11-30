0ac6aaf96fa94068e9cecc0e655617c2564ac3de
Iden <iden1109@gmail.com>
2019-11-30 10:58:03 +0800

    實戰演練:練習 ExpectedCondition

--- src/labs/angulario_search.e2e-spec.ts
+++ src/labs/angulario_search.e2e-spec.ts
@@ -1,4 +1,13 @@
+import { browser, by, element, $, $$, ExpectedConditions as EC } from 'protractor';
 
-describe('the user does search on angular.io', () => {
+fdescribe('the user does search on angular.io', () => {
 
+    it('should search "ngzone"', async () => {
+        await browser.get('https://angular.io/');
+
+        await element(by.tagName('input')).sendKeys('ngzone');
+        browser.wait(EC.visibilityOf($('.search-results')), 5000);
+        
+        expect(await element(by.linkText('NgZone')).isDisplayed()).toBe(true);
+    });
 });
