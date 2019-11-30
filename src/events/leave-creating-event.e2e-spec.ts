388c440d6326f25a4f31467b18e090855df31d55
Iden <iden1109@gmail.com>
2019-11-30 11:55:00 +0800

    實戰演練:練習 window 彈出視窗操作

--- src/events/leave-creating-event.e2e-spec.ts
+++ src/events/leave-creating-event.e2e-spec.ts
@@ -1,4 +1,20 @@
+import { browser, by, element, $, $$, ExpectedConditions as EC, ElementFinder } from 'protractor';
 
 describe('leave add new event page', () => {
 
+    let cancelBtn: ElementFinder;
+
+    beforeAll(async () => {
+        await browser.get('/events/new');
+    });
+
+    beforeEach(() => {
+        cancelBtn = element(by.buttonText('取消'));
+    });
+
+    it('should cancel the event', async () => {
+        await cancelBtn.click();
+        await browser.switchTo().alert().accept();
+        expect(await browser.getCurrentUrl()).toContain('events');
+    });
 });
