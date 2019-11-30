b3e62d9d3d050af3fe1e1dd77c2df0df92eb1ac4
Iden <iden1109@gmail.com>
2019-11-30 11:00:55 +0800

    實戰演練:練習 ExpectedCondition (老師版本)

--- src/labs/angulario_search.e2e-spec.ts
+++ src/labs/angulario_search.e2e-spec.ts
@@ -1,13 +1,31 @@
-import { browser, by, element, $, $$, ExpectedConditions as EC } from 'protractor';
+import { browser, element, by, $, ExpectedConditions as EC } from 'protractor';
 
-fdescribe('the user does search on angular.io', () => {
+describe('the user does search on angular.io', () => {
 
-    it('should search "ngzone"', async () => {
-        await browser.get('https://angular.io/');
+  const searchResultsElement = element(by.className('search-results'));
 
-        await element(by.tagName('input')).sendKeys('ngzone');
-        browser.wait(EC.visibilityOf($('.search-results')), 5000);
-        
-        expect(await element(by.linkText('NgZone')).isDisplayed()).toBe(true);
-    });
+  it('should navigate to angular.io', async () => {
+    await browser.get('https://angular.io/');
+    expect(await browser.getCurrentUrl()).toBe('https://angular.io/');
+  });
+
+  it('should type NgZone on search input', async () => {
+    await $('aio-search-box > input[type="search"]').sendKeys('NgZone');
+
+    const wait = EC.textToBePresentInElement(searchResultsElement, 'NgZone');
+    await browser.wait(wait, 5000, '搜尋結果等待過久!');
+
+    // 如果上面發生 Timeout 意味著找不到 NgZone，且會發生錯誤，不會執行到這一行！
+    const searchResult = await searchResultsElement.getText();
+    expect(searchResult).toContain('NgZone');
+  });
+
+  it('should click NgZone on search result', async () => {
+    await element(by.linkText('NgZone')).click();
+
+    const wait = EC.textToBePresentInElement(element(by.id('ngzone')), 'NgZone');
+    await browser.wait(wait, 5000);
+
+    expect(await element(by.id('ngzone')).isPresent()).toBe(true);
+  });
 });
