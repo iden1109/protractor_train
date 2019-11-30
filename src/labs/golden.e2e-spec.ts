e5fd7913132bd31dee6399d0b3636020f73051ea
Iden <iden1109@gmail.com>
2019-11-30 14:44:05 +0800

    實戰演練:練習畫面呈現測試

--- src/labs/golden.e2e-spec.ts
+++ src/labs/golden.e2e-spec.ts
@@ -1,4 +1,20 @@
+import { browser, by, element, $, $$, ExpectedConditions as EC } from 'protractor';
+import { compareScreenshot } from 'blue-harvest';
 
 describe('main page present', () => {
-
+    
+    it('should compare pages', async () => {
+        await browser.get('/');
+        await browser.manage().window().setSize(1366, 1024);
+        const golden = 'e2e/goldens/home.png';
+        const diffDir = 'e2e/goldens/'; // 會產生 diff-home.png
+        await browser.waitForAngular(); // 截圖前一定要 wait
+        const actual = await browser.takeScreenshot();
+        try {
+            const result = await compareScreenshot(actual, golden, diffDir);
+            expect(result).toBeTruthy();
+        } catch (e) {
+            expect(e).toBeFalsy('首頁 / 比對版面錯誤');
+        }
+    });
 });
